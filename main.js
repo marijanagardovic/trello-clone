c0d3863476b71c4a5b19178409a4e00a83decf9c
marijanagardovic <marijana.gardovic99@gmail.com>
2021-11-01 16:07:27 +0100

    chnaged function for adding cards, must change again

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let input = document.querySelector('#add-one');
 let iconAdd = document.querySelector('#task-submit');
 let addTask = document.querySelector('.task-list');
 let tasks = document.querySelector('.tasks');
+let li = document.createElement('li');
 
 //adding cards
 
@@ -9,8 +10,10 @@ iconAdd.addEventListener('click', () => {
     if(input.value == '') {
         input.value = 'Add a card';
     }
-    addTask.classList.add('active');
-    addTask.innerHTML = input.value;
+    li.appendChild(document.createTextNode(input.value));
+    addTask.appendChild(li);
     
+/*     addTask.classList.add('active');
+    addTask.innerHTML = input.value; */
 })
 
