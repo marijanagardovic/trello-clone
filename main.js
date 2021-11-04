23b2884799f237984a049eeca2b37a9750c395a5
marijanagardovic <marijana.gardovic99@gmail.com>
2021-11-04 11:07:31 +0100

    made some changes

--- main.js
+++ main.js
@@ -9,37 +9,37 @@ const close_btns = document.querySelectorAll(".close");
 //create drag and drop functions
 tasks.forEach(task => {
     task.addEventListener('dragstart', () => {
-      task.classList.add('dragging')
+      task.classList.add('dragging');
     })
   
     task.addEventListener('dragend', () => {
-      task.classList.remove('dragging')
+      task.classList.remove('dragging');
     })
 })
   
 columns.forEach(column => {
       column.addEventListener('dragover', e => {
-      e.preventDefault()
-      const afterElement = getDragAfterElement(column, e.clientY)
-      const draggable = document.querySelector('.dragging')
+      e.preventDefault();
+      const afterElement = getDragAfterElement(column, e.clientY);
+      const draggable = document.querySelector('.dragging');
       if (afterElement == null) {
-        column.appendChild(draggable)
+        column.appendChild(draggable);
       } else {
-        column.insertBefore(draggable, afterElement)
+        column.insertBefore(draggable, afterElement);
       }
     })
 })
   
 function getDragAfterElement(column, y) {
 
-    const draggableElements = [...column.querySelectorAll('.tasks:not(.dragging)')]
+    const draggableElements = [...column.querySelectorAll('.tasks:not(.dragging)')];
     return draggableElements.reduce((closest, child) => {
-      const box = child.getBoundingClientRect()
-      const offset = y - box.top - box.height / 2
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
       if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child }
+        return { offset: offset, element: child };
       } else {
-        return closest
+        return closest;
       }
     }, { offset: Number.NEGATIVE_INFINITY }).element
 }
